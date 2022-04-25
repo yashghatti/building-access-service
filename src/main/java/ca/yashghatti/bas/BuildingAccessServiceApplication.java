eb07c13a288b889e20cf4c549356fe2571a01643
Yash Ghatti <yash.ghatti@fcc-fac.ca>
2022-04-24 21:00:12 -0600

    Switched to java because fuck expressjs and its fuckall performance

---
+++ src/main/java/ca/yashghatti/bas/BuildingAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.bas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingAccessServiceApplication.class, args);
+	}
+
+}
