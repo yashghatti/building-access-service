ad29b2a1338e77ae90e089f462310a9205bed373
Yash Ghatti <yash.ghatti@fcc-fac.ca>
2022-05-06 18:14:21 -0600

    Added a homepage

---
+++ src/main/java/ca/yashghatti/bas/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package ca.yashghatti.bas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:static/");
+
+    }
+
+
+}
